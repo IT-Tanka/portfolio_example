5c29958ccf2a4a501f1fe351c1aad8e4d2e6c070
Tanya <72666478+IT-Tanka@users.noreply.github.com>
2022-02-11 15:22:19 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,40 @@
+new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.portfolio__arrow_right',
+    prevEl: '.portfolio__arrow_left',
+    disabledClass:'..portfolio__arrow_disable',
+  },
+  breakpoints: {
+    
+    // when window width is >= 480px
+    480: {
+      grid:{
+        fill:'row',
+        rows:2
+      }
+    },
+    // when window width is >= 640px
+    768: {
+      slidesPerView: 2,
+      grid:{
+        fill:'row',
+        rows:2
+      }
+    }
+  }
+});
+$('.header__contacts-burger').click(function(){
+  $('.header__contacts').slideToggle();
+});
+// $('.present__order-btn').click(()=>{
+//   $('.page__overlay_modal').fadeIn(400).css('display', 'flex');
+// })
+// $('.modal__close').click(()=>{
+//   $('.page__overlay_modal').fadeOut(400);
+// })
+$('.present__order-btn').click(()=>{
+  $('.page__overlay_modal').show(200);
+})
+$('.modal__close').click(()=>{
+  $('.page__overlay_modal').hide(200);
+})
